52e44e5a0a7fcf83ca053ef5973c0e228123d1e8
Xenthio <thegooster88@gmail.com>
2024-10-11 10:47:36 +1100

    Functioning spam filter

--- Moderation/SpamFilter.cs
+++ Moderation/SpamFilter.cs
@@ -7,10 +7,64 @@ namespace OpenRobo.Moderation;
 
 internal class SpamFilter
 {
+	public static Dictionary<ulong, int> UserSpamCounter = new();
+	public static Dictionary<ulong, int> UserMuteCounter = new();
 	[Events.MessageRecieved]
 	public static void MessageRecieved(SocketMessage socketMessage)
 	{
+		if (socketMessage.Channel is SocketGuildChannel channel)
+		{
+			var guild = channel.Guild;
+			var serverInstance = ServerInstance.GetOrCreateServerInstance(guild);
+			if (serverInstance.Config.SpamFilteredChannels.Contains(socketMessage.Channel.Id) && socketMessage.Author is SocketGuildUser user)
+			{
+				var userid = socketMessage.Author.Id;
+				CountCooldown(userid);
+				if (UserSpamCounter.ContainsKey(userid) && UserSpamCounter[userid] > GetCooldownThresholdForUser(userid))
+				{
+					Mutes.MuteUser(user, GetCooldownTimeForUser(userid));
+
+				}
+			}
+		}
 	}
+	public static async Task CountCooldown(ulong userid)
+	{
+		if (!UserSpamCounter.ContainsKey(userid))
+			UserSpamCounter[userid] = 1;
+		else
+			UserSpamCounter[userid] += 1;
+		Log.Verbose($"spam filter counter for {userid}: {UserSpamCounter[userid]}");
+		await Task.Delay(2 * 1000);
+		UserSpamCounter[userid] -= 1;
+		if (UserSpamCounter[userid] <= 0) UserSpamCounter.Remove(userid);
+	}
+
+	public static int GetCooldownThresholdForUser(ulong userid)
+	{
+		// Todo: bias with user level
+		return 5;
+	}
+	public static async Task CountMute(ulong userid)
+	{
+		if (!UserMuteCounter.ContainsKey(userid))
+			UserMuteCounter[userid] = 1;
+		else
+			UserMuteCounter[userid] += 1;
+		Log.Verbose($"spam filter mute counter for {userid}: {UserMuteCounter[userid]}");
+		await Task.Delay(120 * 1000);
+		UserMuteCounter[userid] -= 1;
+		if (UserMuteCounter[userid] <= 0) UserMuteCounter.Remove(userid);
+	}
+	public static int GetCooldownTimeForUser(ulong userid)
+	{
+		// Todo: bias with recent mutes in last 2 minutes (see function CountMute())
+		return 30;
+	}
+	/// <summary>
+	/// Toggle or view the state of the spam filter for this channel.
+	/// </summary>
+	/// <param name="socketMessage"></param>
 	[ChatCommand("spamfilter")]
 	public static void SpamFilterCommand(SocketMessage socketMessage)
 	{
