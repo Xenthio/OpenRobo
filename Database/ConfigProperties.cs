7606088e965d99db3abc7de403f1b27e7ccf32f4
Xenthio <thegooster88@gmail.com>
2024-10-18 10:40:22 +1100

    Hopefully Proper settings menu

--- Database/ConfigProperties.cs
+++ Database/ConfigProperties.cs
@@ -7,9 +7,9 @@ public class ConfigType
 	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
 	public class NumberBoxAttribute : System.Attribute { }
 	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
-	public class ChannelSelection : System.Attribute { }
+	public class ChannelSelectionAttribute : System.Attribute { }
 	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
-	public class ChannelList : System.Attribute { }
+	public class ChannelListAttribute : System.Attribute { }
 	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
 	public class RoleSelectionAttribute : System.Attribute { }
 	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
